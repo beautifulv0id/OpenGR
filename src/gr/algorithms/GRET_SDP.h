b0a875935a819e7b7d0f9e46795d57191f072422
beautifulv0id <felix.rodau@gmail.com>
2020-05-04 18:42:53 +0200

    Implement basic structure of ComputeTransformations function in GRET-SDP

---
+++ src/gr/algorithms/GRET_SDP.h
@@ -0,0 +1,85 @@
+#ifndef _OPENGR_ALGO_GRET_SDP_
+#define _OPENGR_ALGO_GRET_SDP_
+
+#include <vector>
+
+#ifdef OpenGR_USE_OPENMP
+#include <omp.h>
+#endif
+
+#include "gr/shared.h"
+#include "gr/algorithms/NAryMatchBase.h"
+
+#ifdef TEST_GLOBAL_TIMINGS
+#   include "gr/utils/timer.h"
+#endif
+
+namespace gr{
+
+
+template < class Derived, class TBase>
+class GRETSDPOptions : public  TBase
+{
+public:
+    using Scalar = typename TBase::Scalar;
+
+};
+
+
+/// \brief Base class for Congruent Sec Exploration algorithms
+/// \tparam _Traits Defines properties of the Base used to build the congruent set.
+template <typename _PointType,
+          typename _TransformVisitor,
+          template < class, class > class ... OptExts >
+class GRET_SDP : public NAryMatchBase<_PointType, _TransformVisitor, OptExts ..., GRETSDPOptions> {
+
+public:
+    using TransformVisitor = _TransformVisitor;
+
+    using MatchBaseType = NAryMatchBase<_PointType, _TransformVisitor, OptExts ..., GRETSDPOptions>;
+    using PosMutablePoint = typename MatchBaseType::PosMutablePoint;
+    using OptionsType = typename MatchBaseType::OptionsType;
+
+    using Scalar = typename MatchBaseType::Scalar;
+    using VectorType = typename MatchBaseType::VectorType;
+    using MatrixType = typename MatchBaseType::MatrixType;
+
+    using LogLevel = typename MatchBaseType::LogLevel;
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+
+    GRET_SDP(const OptionsType& options
+            , const Utils::Logger &logger
+    );
+
+    virtual ~GRET_SDP();
+
+    template <typename InputRange, template<typename> typename Sampler>  
+    void ComputeTransformations(const std::vector<InputRange>& P,
+                                int N_,
+                                int M_,
+                                int d_,
+                                Eigen::MatrixXd& L,
+                                std::vector<MatrixType>& transformations_,
+                                std::vector<VectorType>& translations_,
+                                const Sampler<_PointType>& sampler,
+                                TransformVisitor& v);
+
+    private:
+    // dimension, currently set to 3
+    int d;
+    // number of points
+    int N;
+    // number of patches
+    int M;
+
+    template <typename InputRange>
+    void ComputeMatricesBDC(const std::vector<InputRange>& P, 
+                            Eigen::MatrixXd& L);
+
+}; /// class GRET_SDP
+} /// namespace gr
+#include "GRET_SDP.hpp"
+
+#endif // _OPENGR_ALGO_GRET_SDP_
---
+++ src/gr/algorithms/NAryMatchBase.h
@@ -0,0 +1,70 @@
+
+#ifndef _OPENGR_ALGO_N_ARY_MATCH_BASE_
+#define _OPENGR_ALGO_N_ARY_MATCH_BASE_
+
+#include <vector>
+
+#ifdef OpenGR_USE_OPENMP
+#include <omp.h>
+#endif
+
+#include "gr/algorithms/AbstractMatchBase.h"
+#include "gr/shared.h"
+#include "gr/sampling.h"
+#include "gr/accelerators/kdtree.h"
+#include "gr/utils/logger.h"
+#include "gr/utils/crtp.h"
+
+#ifdef TEST_GLOBAL_TIMINGS
+#   include "gr/utils/timer.h"
+#endif
+
+namespace gr{
+
+template < class Derived, class TBase>
+    class NAryMatchOptions : public TBase
+    {
+    public:
+        using Scalar = typename TBase::Scalar;
+
+        // n-ary matching options here
+    };
+
+
+/// \brief Abstract class for n-ary registration algorithms (multiple point clouds)
+template   <typename PointType, 
+            typename _TransformVisitor,
+            template < class, class > typename ... OptExts>
+class NAryMatchBase : public AbstractMatchBase<PointType, _TransformVisitor, OptExts ..., NAryMatchOptions> {
+
+public:
+    using MatchBaseType = AbstractMatchBase<PointType, _TransformVisitor, OptExts ..., NAryMatchOptions>;
+
+    using Scalar = typename MatchBaseType::Scalar;
+    using VectorType = typename MatchBaseType::VectorType;
+    using MatrixType = typename MatchBaseType::MatrixType;
+    using LogLevel = typename MatchBaseType::LogLevel;
+    using OptionsType = typename MatchBaseType::OptionsType;
+    using PosMutablePoint = typename MatchBaseType::PosMutablePoint;
+    using TransformVisitor = _TransformVisitor;
+
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    NAryMatchBase(const OptionsType& options, const Utils::Logger &logger);
+
+    virtual ~NAryMatchBase();
+
+
+
+
+    protected:
+    /// vector of the transformations
+    std::vector<Eigen::Matrix<Scalar, 4, 4>> transforms_;
+    
+
+}; /// class NAryMatchBase
+} /// namespace gr
+#include "NAryMatchBase.hpp"
+
+#endif // _OPENGR_ALGO_N_ARY_MATCH_BASE_
